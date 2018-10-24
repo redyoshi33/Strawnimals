e21cf8b2130e3b31249abaadd75e4c43c9da8e37
redyoshi33 <ryoung1993@yahoo.com>
2018-10-23 17:00:39 -0700

    changed bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const path = require("path");
 const multer = require("multer")
